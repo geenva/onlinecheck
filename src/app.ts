7077cb036adce242e65a7bbd09edbb1f66a30e98
geenva <geenva@users.noreply.github.com>
2021-04-05 22:25:46 +0800

    init

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import path from "path";
+const app = express();
+
+const router = express.Router();
+
+router.get("/", function (req, res) {
+  console.log("Serving / to user.");
+  res.sendFile(path.join(__dirname + "/../views/index.html"));
+});
+
+router.get("/api", (req, res, next) => {
+  console.log("Serving API to user.");
+  res.json({
+    status: "200",
+    content:
+      "Yes, this is working! But you see, we're not quite sure what to do with this API just yet. See you soon, in the future!",
+  });
+});
+
+app.use("/", router);
+app.listen(process.env.port || "8080");
+console.log("Listening, loud and clear.");
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-////////////////////////////////////////////
-/////         Create Discord App       /////
-////////////////////////////////////////////
-
 import * as dotenv from "dotenv";
-dotenv.config();
+dotenv.config({ path: "../.env" });
+import express from "express";
+import path from "path";
 
 import Discord from "discord.js";
 
@@ -16,11 +14,36 @@ class Client extends Discord.Client {}
 
 const client = new Client();
 
-// basic events
 client.on("ready", () => {
-  console.log("Bot is online!");
+  console.log("Bot | Ready!");
 });
+
 client.on("warn", console.warn);
 client.on("error", console.error);
 
 client.login(config.TOKEN);
+
+setTimeout(function () {
+  const user = client.users.cache.get("457805013474082817");
+
+  const app = express();
+
+  const router = express.Router();
+
+  router.get("/", function (req, res) {
+    console.log("Serving / to user.");
+    res.sendFile(path.join(__dirname + "/../views/index.html"));
+  });
+
+  router.get("/api", (req, res, next) => {
+    console.log("Serving API to user.");
+    res.json({
+      status: "200",
+      content: user.presence.status,
+    });
+  });
+
+  app.use("/", router);
+  app.listen(process.env.port || "8080");
+  console.log("Listening, loud and clear.");
+}, 3000);
